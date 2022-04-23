8255167e2324072a52a8462d28e5947a6a230314
Barun Debnath <barundebnath91@gmail.com>
2022-04-23 18:08:19 +0530

    static profile page done

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,4 +1,10 @@
 import { useParams } from "react-router-dom";
+import BioCard from "./BioCard";
+import ProfileCard from "./ProfileCard";
+import SubscribeCard from "./SubscribeCard";
+import ContentFilterButton from "./ContentFilterButton";
+import PostCard from "./PostCard";
+import SubscribeCTA from "./SubscribeCTA";
 
 const Profile = () => {
 	const { username } = useParams();
@@ -7,6 +13,16 @@ const Profile = () => {
 		<>
 			<main className="container">
 				<h1>Hello {username}</h1>
+				<ProfileCard />
+				<BioCard />
+				<SubscribeCard />
+				<ContentFilterButton />
+				<div className="mt-5 grid grid-cols-1 gap-4 lg:grid-cols-3">
+					<PostCard />
+					<PostCard />
+					<PostCard />
+				</div>
+				<SubscribeCTA />
 			</main>
 		</>
 	);
